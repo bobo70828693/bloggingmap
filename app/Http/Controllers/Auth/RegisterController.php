88dfb8f033d17b2d4112583807ae14a96a7984c7
bobo70828693 <bobo70828693@gmail.com>
2020-03-04 23:12:46 +0800

    add login function

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Hashid;
 
 class RegisterController extends Controller
 {
@@ -67,6 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'token' => Hash::make($data['name']),
             'password' => Hash::make($data['password']),
         ]);
     }
